985280426de3cd4531623854e7bf00cda2864bcb
Mashhood Rastgar <mashhoodr@gmail.com>
2013-12-17 11:55:57 +0500

    added a bash script to run the passed APK on all available devices, creating output folders for each device separately

---
+++ runtests.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# To run on all devices use ./runtests PATH_TO_APK
+
+#variables
+APK_PATH=$1
+
+echo "Lauching the tests on all available devices..\n"
+
+DEVICES=`adb devices`
+for DEVICE in ${DEVICES}
+do
+  # DEVICE serials are 16 chars, rest all is junk..
+  if [ "${#DEVICE}" -eq "16" ]
+  then
+    echo "Launching tests on DEVICE: $DEVICE"
+    OUTPUT_PATH="testoutput/$DEVICE"
+    mkdir -p $OUTPUT_PATH
+    SCREENSHOT_PATH="$OUTPUT_PATH/" ADB_DEVICE_ARG=$DEVICE calabash-android run $APK_PATH --format json --out "$OUTPUT_PATH/output.json" &
+  fi
+done
+
+echo "\nLaunched all tests, waiting for them to finish..."
+wait
+echo "All the tests have finished.."
