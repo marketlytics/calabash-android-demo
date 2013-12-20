32c2f6a121e3fb3398a0a62eb7625e4ff253cdea
Mashhood Rastgar <mashhoodr@gmail.com>
2013-12-20 09:56:53 +0500

    improved the bash script to support devices with serials which are not exactly equal to 16. Also added support for HTML output

--- runtests.sh
+++ runtests.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # To run on all devices use ./runtests PATH_TO_APK
 
@@ -7,17 +7,12 @@ APK_PATH=$1
 
 echo "Lauching the tests on all available devices..\n"
 
-DEVICES=`adb devices`
-for DEVICE in ${DEVICES}
+for DEVICE in `adb devices | grep -v "List" | awk '{print $1}'`
 do
-  # DEVICE serials are 16 chars, rest all is junk..
-  if [ "${#DEVICE}" -eq "16" ]
-  then
-    echo "Launching tests on DEVICE: $DEVICE"
-    OUTPUT_PATH="testoutput/$DEVICE"
-    mkdir -p $OUTPUT_PATH
-    RESET_BETWEEN_SCENARIOS=1 SCREENSHOT_PATH="$OUTPUT_PATH/" ADB_DEVICE_ARG=$DEVICE calabash-android run $APK_PATH --format json --out "$OUTPUT_PATH/output.json" &
-  fi
+  echo "Launching tests on DEVICE: $DEVICE"
+  OUTPUT_PATH="testoutput/$DEVICE"
+  mkdir -p $OUTPUT_PATH
+  RESET_BETWEEN_SCENARIOS=1 SCREENSHOT_PATH="$OUTPUT_PATH/" ADB_DEVICE_ARG=$DEVICE calabash-android run $APK_PATH --format html --out "$DEVICE.html" &
 done
 
 echo "\nLaunched all tests, waiting for them to finish..."
